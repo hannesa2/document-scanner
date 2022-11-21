5a5dfddd05542206acee00a9562e146dc9da495e
Hannes Achleitner <hannes.software@gmx.at>
2022-11-21 09:00:32 +0100

    Screenshot tests

--- app/src/androidTest/java/info/hannes/documentscanner/demo/SmokeTest.kt
+++ app/src/androidTest/java/info/hannes/documentscanner/demo/SmokeTest.kt
@@ -1,31 +1,50 @@
 package info.hannes.documentscanner.demo
 
 import android.Manifest
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import android.util.Log
+import androidx.test.core.app.takeScreenshot
+import androidx.test.core.graphics.writeToTestStorage
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.screenshot.captureToBitmap
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.GrantPermissionRule
-import com.moka.utils.Screenshot
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestName
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SmokeTest {
 
     @get:Rule
-    var activityScenarioRule = ActivityScenarioRule(PreviewActivity::class.java)
+    val activityScenarioRule = activityScenarioRule<PreviewActivity>()
+
+    // a handy JUnit rule that stores the method name, so it can be used to generate unique screenshot files per test method
+    @get:Rule
+    var nameRule = TestName()
 
     @get:Rule
     val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.CAMERA,
-            Manifest.permission.READ_EXTERNAL_STORAGE)
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
 
     @Test
     fun smokeTestSimplyStart() {
-        for (i in 1000L..5000L step 500) {
+        for (i in 3000L..5000L step 1000) {
             Thread.sleep(i)
-            Screenshot.takeScreenshot("$i")
+            takeScreenshot()
+                .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-S$i")
+            try {
+                Espresso.onView(ViewMatchers.isRoot())
+                    .captureToBitmap()
+                    .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-A$i")
+            } catch (e: Exception) {
+                Log.e("smokeTestSimplyStart", e.message.toString())
+            }
         }
     }
 }
--- app/src/androidTest/java/info/hannes/documentscanner/demo/StartTest.kt
+++ app/src/androidTest/java/info/hannes/documentscanner/demo/StartTest.kt
@@ -1,28 +1,31 @@
 package info.hannes.documentscanner.demo
 
-import android.Manifest
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.core.graphics.writeToTestStorage
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.screenshot.captureToBitmap
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.GrantPermissionRule
-import com.moka.utils.Screenshot
 import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestName
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class StartTest {
 
     @get:Rule
-    var activityScenarioRule = ActivityScenarioRule(StartActivity::class.java)
+    val activityScenarioRule = activityScenarioRule<StartActivity>()
 
+    // a handy JUnit rule that stores the method name, so it can be used to generate unique screenshot files per test method
     @get:Rule
-    val grantPermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE)
+    var nameRule = TestName()
 
     @Test
     fun smokeTestSimplyStart() {
         Thread.sleep(1000)
-        Screenshot.takeScreenshot("End")
+        Espresso.onView(ViewMatchers.isRoot())
+            .captureToBitmap()
+            .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}")
     }
 }
