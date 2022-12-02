ff30f1dd739fc57bcdec0298e1330f7790144c04
Hannes Achleitner <hannes.software@gmx.at>
2022-12-02 12:35:25 +0100

    Change coroutine

--- library/src/main/java/info/hannes/documentscanner/domain/UseCase.kt
+++ library/src/main/java/info/hannes/documentscanner/domain/UseCase.kt
@@ -1,18 +1,15 @@
 package info.hannes.documentscanner.domain
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import info.hannes.documentscanner.support.Either
+import kotlinx.coroutines.*
 
 abstract class UseCase<Type, in Params> where Type : Any? {
 
     abstract suspend fun run(params: Params): Either<Failure, Type>
 
     operator fun invoke(params: Params, onResult: (Either<Failure, Type>) -> Unit = {}) {
-        val job = GlobalScope.async(Dispatchers.IO) { run(params) }
-        GlobalScope.launch(Dispatchers.Main) { onResult(job.await()) }
+        val job = CoroutineScope(Dispatchers.IO).async { run(params) }
+        CoroutineScope(Dispatchers.Main).launch { onResult(job.await()) }
     }
 }
 
