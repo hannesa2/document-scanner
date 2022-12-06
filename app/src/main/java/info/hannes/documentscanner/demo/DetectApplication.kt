fa0f39e7fdf31847161c7e3555365244af51c883
Hannes Achleitner <hannes.software@gmx.at>
2022-12-06 06:42:14 +0100

    Button to test Crashlytics

--- app/src/main/java/info/hannes/documentscanner/demo/DetectApplication.kt
+++ app/src/main/java/info/hannes/documentscanner/demo/DetectApplication.kt
@@ -1,23 +1,15 @@
 package info.hannes.documentscanner.demo
 
 import android.annotation.SuppressLint
-import android.app.Application
+import info.hannes.logcat.LoggingApplication
 import info.hannes.timber.FileLoggingTree
 import timber.log.Timber
 
-class DetectApplication : Application() {
+class DetectApplication : LoggingApplication() {
 
     @SuppressLint("HardwareIds")
     override fun onCreate() {
         super.onCreate()
-
-        val oldHandler = Thread.getDefaultUncaughtExceptionHandler()
-
-        Thread.setDefaultUncaughtExceptionHandler { t, e ->
-            @Suppress("ControlFlowWithEmptyBody")
-            Timber.e(e.cause?.also { } ?: run { e })
-            oldHandler?.uncaughtException(t, e)
-        }
 
         externalCacheDir?.let {
             Timber.plant(FileLoggingTree(it, this))
